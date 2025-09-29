bb7defea2d2aba6aad3ff0d706d4ba80a99f66ff
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-09-29 11:05:22 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewBag_storage.Models;
+
+namespace ViewBag_storage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string name)
+        {
+            // store to viewbag
+            ViewBag.user = name;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewBag_storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
